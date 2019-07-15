037f3625a389ba2d572f54bdf5e7b2ea74232458
lhjhuany <80454541@qq.com>
2019-07-15 14:17:44 +0800

    Update PutRow.py

--- Examples/PutRow.py
+++ Examples/PutRow.py
@@ -5,5 +5,14 @@ if __name__ == '__main__':
     o = TenAwsTSSvc("Key", "秘钥", '区域') # 这3个参数都需要替换成自己相对应的数据
     o.DoConnect()
     
-    TestDeleteRow(o)
+    Item = {
+            "Id": {
+                    "S":"2"
+            },
+            "Arr": {
+                "S": "2"
+            }
+        }
+    putrow = PutRow(client, "Test", Item)
+    b = putrow.DoPutRow()
     
